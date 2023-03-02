0089d2f9992fba335a6223e1f6d943eb911062fa
Debargha-arch <mukherjeedebargha281@gmail.com>
2023-03-02 14:56:55 +0530

    React app added and basic skeleton built

---
+++ src/Views/Body.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from '../Components/About'
+import Events from '../Components/Events'
+import Home from '../Components/Home'
+import Navbar from '../Components/Navbar'
+
+const Body = () => {
+  return (
+    <div>
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/home' element={<Home />}></Route>
+                <Route path='/events' element={<Events />}></Route>
+                <Route path='/about' element={<About />}></Route>
+            </Routes>
+        </BrowserRouter>
+    </div>
+  )
+}
+
+export default Body
