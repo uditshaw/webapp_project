4c7886c46f4f60c3e77af64b2188f9d780436592
Debargha-arch <mukherjeedebargha281@gmail.com>
2023-03-06 11:55:20 +0530

    UI Design updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,13 +2,22 @@ import { Button, ButtonGroup } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const button ={
+  backgroundColor: 'inherit',
+  margin: '0px 20px 0px 20px'
+};
+const buttongroup = {
+  display: 'flex',
+  justifyContent: 'space-around'
+}
+
 const Navbar = () => {
   return (
     <div>
-      <ButtonGroup variant='contained'>
-        <Button><Link to="/home">Home</Link></Button>
-        <Button><Link to="/events">Events</Link></Button>
-        <Button><Link to="/about">About</Link></Button>
+      <ButtonGroup sx={buttongroup}>
+        <Link to="/home" style={{ textDecoration: 'none' }}><Button variant='contained' sx={button}>Home</Button></Link>
+        <Link to="/events" style={{ textDecoration: 'none' }}><Button variant='contained' sx={button}>Events</Button></Link>
+        <Link to="/about" style={{ textDecoration: 'none' }}><Button variant='contained' sx={button}>About</Button></Link>
       </ButtonGroup>
     </div>
   )
