cfd045bc3c52da1c8265b003db2f922173042f31
Udit Shaw <uditshaw17@gmail.com>
2023-03-11 12:37:20 +0530

    Modal on card component implementation left

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,84 +1,172 @@
-import { Button, Card, CardActions, CardContent, CardMedia, List, Paper, Typography } from '@mui/material'
-import React from 'react'
+import {
+  Button,
+  ButtonBase,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  List,
+  Paper,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import ModalDialog from "./Modal_dialog";
 
 const paper = {
-  display: 'flex',
-  flexDirection: 'column',
-  backgroundColor: 'inherit',
-  maxHeight: '100%',
-  overflow: 'auto',
+  display: "flex",
+  flexDirection: "column",
+  backgroundColor: "inherit",
+  maxHeight: "100%",
+  overflow: "auto",
 };
-const list = {
-}
+const list = {};
 
 const card = {
-  display: 'flex',
-  justifyContent: 'flex-start',
-  height: '30vh',
-  width: '95vw',
-  margin: '1.5vh 1vh 1vh 1vh',
+  display: "flex",
+  justifyContent: "flex-start",
+  height: "30vh",
+  width: "95vw",
+  margin: "1.5vh 1vh 1vh 1vh",
+  cursor: "pointer",
 };
 
 const cardmedia = {
-  width: '25vw'
+  width: "25vw",
 };
-const cardcontent ={
-  width: '60vw',
+const cardcontent = {
+  width: "60vw",
 };
 const cardactions = {
-  width: '5vw',
-}
+  width: "5vw",
+};
+
+const modelOpen = () => {
+  <ModalDialog open={true} />;
+};
 
 const Events = () => {
   return (
     <div>
-      <Typography variant='h5'>Upcoming Events</Typography>
+      <Typography variant="h5">Upcoming Events</Typography>
       <Paper sx={paper}>
+        <ModalDialog />
         <List sx={list}>
-        <Card sx={card}>
-          <CardMedia sx={cardmedia} component='img' image='https://cdn.kiit.ac.in/wp-content/uploads/2019/11/fest6-700x441.jpg' alt='event1'></CardMedia>
-          <CardContent sx={cardcontent}>
-          <Typography variant='h6'>KIIT-FEST</Typography>
-          <Typography variant='body1'>An annual festival organized for 5 days.It is one of the largest Festivals of the Eastern paradise of India. The year 2019 will witness the 6th edition of KIITFEST and shall take place from 16th to 18th of December with the Theme, ‘TRIBAL PRIDE’ to make more people aware of the rich history and cultural significance of the tribes in India.</Typography>
-          </CardContent>
-          <CardActions sx={cardactions}><Button>VIEW DETAILS</Button></CardActions>
-        </Card>
-        <Card sx={card}>
-          <CardMedia sx={cardmedia} component='img' image='https://cdn.kiit.ac.in/wp-content/uploads/2019/11/fest6-700x441.jpg' alt='event1'></CardMedia>
-          <CardContent sx={cardcontent}>
-          <Typography variant='h6'>KIIT-FEST</Typography>
-          <Typography variant='body1'>An annual festival organized for 5 days.It is one of the largest Festivals of the Eastern paradise of India. The year 2019 will witness the 6th edition of KIITFEST and shall take place from 16th to 18th of December with the Theme, ‘TRIBAL PRIDE’ to make more people aware of the rich history and cultural significance of the tribes in India.</Typography>
-          </CardContent>
-          <CardActions sx={cardactions}><Button>VIEW DETAILS</Button></CardActions>
-        </Card>
-        <Card sx={card}>
-          <CardMedia sx={cardmedia} component='img' image='https://cdn.kiit.ac.in/wp-content/uploads/2019/11/fest6-700x441.jpg' alt='event1'></CardMedia>
-          <CardContent sx={cardcontent}>
-          <Typography variant='h6'>KIIT-FEST</Typography>
-          <Typography variant='body1'>An annual festival organized for 5 days.It is one of the largest Festivals of the Eastern paradise of India. The year 2019 will witness the 6th edition of KIITFEST and shall take place from 16th to 18th of December with the Theme, ‘TRIBAL PRIDE’ to make more people aware of the rich history and cultural significance of the tribes in India.</Typography>
-          </CardContent>
-          <CardActions sx={cardactions}><Button>VIEW DETAILS</Button></CardActions>
-        </Card>
-        <Card sx={card}>
-          <CardMedia sx={cardmedia} component='img' image='https://cdn.kiit.ac.in/wp-content/uploads/2019/11/fest6-700x441.jpg' alt='event1'></CardMedia>
-          <CardContent sx={cardcontent}>
-          <Typography variant='h6'>KIIT-FEST</Typography>
-          <Typography variant='body1'>An annual festival organized for 5 days.It is one of the largest Festivals of the Eastern paradise of India. The year 2019 will witness the 6th edition of KIITFEST and shall take place from 16th to 18th of December with the Theme, ‘TRIBAL PRIDE’ to make more people aware of the rich history and cultural significance of the tribes in India.</Typography>
-          </CardContent>
-          <CardActions sx={cardactions}><Button>VIEW DETAILS</Button></CardActions>
-        </Card>
-        <Card sx={card}>
-          <CardMedia sx={cardmedia} component='img' image='https://cdn.kiit.ac.in/wp-content/uploads/2019/11/fest6-700x441.jpg' alt='event1'></CardMedia>
-          <CardContent sx={cardcontent}>
-          <Typography variant='h6'>KIIT-FEST</Typography>
-          <Typography variant='body1'>An annual festival organized for 5 days.It is one of the largest Festivals of the Eastern paradise of India. The year 2019 will witness the 6th edition of KIITFEST and shall take place from 16th to 18th of December with the Theme, ‘TRIBAL PRIDE’ to make more people aware of the rich history and cultural significance of the tribes in India.</Typography>
-          </CardContent>
-          <CardActions sx={cardactions}><Button>VIEW DETAILS</Button></CardActions>
-        </Card>
+          <Card sx={card} onClick={modelOpen}>
+            {/* <ButtonBase onClick={}> */}
+            <CardMedia
+              sx={cardmedia}
+              component="img"
+              image="https://cdn.kiit.ac.in/wp-content/uploads/2019/11/fest6-700x441.jpg"
+              alt="event1"
+            ></CardMedia>
+            <CardContent sx={cardcontent}>
+              <Typography variant="h6">KIIT-FEST</Typography>
+              <Typography variant="body1">
+                An annual festival organized for 5 days.It is one of the largest
+                Festivals of the Eastern paradise of India. The year 2019 will
+                witness the 6th edition of KIITFEST and shall take place from
+                16th to 18th of December with the Theme, ‘TRIBAL PRIDE’ to make
+                more people aware of the rich history and cultural significance
+                of the tribes in India.
+              </Typography>
+            </CardContent>
+            {/* </ButtonBase> */}
+            <CardActions sx={cardactions}>
+              <Button>VIEW DETAILS</Button>
+            </CardActions>
+          </Card>
+          <Card sx={card}>
+            <CardMedia
+              sx={cardmedia}
+              component="img"
+              image="https://cdn.kiit.ac.in/wp-content/uploads/2019/11/fest6-700x441.jpg"
+              alt="event1"
+            ></CardMedia>
+            <CardContent sx={cardcontent}>
+              <Typography variant="h6">KIIT-FEST</Typography>
+              <Typography variant="body1">
+                An annual festival organized for 5 days.It is one of the largest
+                Festivals of the Eastern paradise of India. The year 2019 will
+                witness the 6th edition of KIITFEST and shall take place from
+                16th to 18th of December with the Theme, ‘TRIBAL PRIDE’ to make
+                more people aware of the rich history and cultural significance
+                of the tribes in India.
+              </Typography>
+            </CardContent>
+            <CardActions sx={cardactions}>
+              <Button>VIEW DETAILS</Button>
+            </CardActions>
+          </Card>
+          <Card sx={card}>
+            <CardMedia
+              sx={cardmedia}
+              component="img"
+              image="https://cdn.kiit.ac.in/wp-content/uploads/2019/11/fest6-700x441.jpg"
+              alt="event1"
+            ></CardMedia>
+            <CardContent sx={cardcontent}>
+              <Typography variant="h6">KIIT-FEST</Typography>
+              <Typography variant="body1">
+                An annual festival organized for 5 days.It is one of the largest
+                Festivals of the Eastern paradise of India. The year 2019 will
+                witness the 6th edition of KIITFEST and shall take place from
+                16th to 18th of December with the Theme, ‘TRIBAL PRIDE’ to make
+                more people aware of the rich history and cultural significance
+                of the tribes in India.
+              </Typography>
+            </CardContent>
+            <CardActions sx={cardactions}>
+              <Button>VIEW DETAILS</Button>
+            </CardActions>
+          </Card>
+          <Card sx={card}>
+            <CardMedia
+              sx={cardmedia}
+              component="img"
+              image="https://cdn.kiit.ac.in/wp-content/uploads/2019/11/fest6-700x441.jpg"
+              alt="event1"
+            ></CardMedia>
+            <CardContent sx={cardcontent}>
+              <Typography variant="h6">KIIT-FEST</Typography>
+              <Typography variant="body1">
+                An annual festival organized for 5 days.It is one of the largest
+                Festivals of the Eastern paradise of India. The year 2019 will
+                witness the 6th edition of KIITFEST and shall take place from
+                16th to 18th of December with the Theme, ‘TRIBAL PRIDE’ to make
+                more people aware of the rich history and cultural significance
+                of the tribes in India.
+              </Typography>
+            </CardContent>
+            <CardActions sx={cardactions}>
+              <Button>VIEW DETAILS</Button>
+            </CardActions>
+          </Card>
+          <Card sx={card}>
+            <CardMedia
+              sx={cardmedia}
+              component="img"
+              image="https://cdn.kiit.ac.in/wp-content/uploads/2019/11/fest6-700x441.jpg"
+              alt="event1"
+            ></CardMedia>
+            <CardContent sx={cardcontent}>
+              <Typography variant="h6">KIIT-FEST</Typography>
+              <Typography variant="body1">
+                An annual festival organized for 5 days.It is one of the largest
+                Festivals of the Eastern paradise of India. The year 2019 will
+                witness the 6th edition of KIITFEST and shall take place from
+                16th to 18th of December with the Theme, ‘TRIBAL PRIDE’ to make
+                more people aware of the rich history and cultural significance
+                of the tribes in India.
+              </Typography>
+            </CardContent>
+            <CardActions sx={cardactions}>
+              <Button>VIEW DETAILS</Button>
+            </CardActions>
+          </Card>
         </List>
       </Paper>
     </div>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
