cc123fb34e8ddac8ceaf1f247d1b33a29f39a38b
aryanani <72939789+aryanani@users.noreply.github.com>
2023-04-05 17:48:37 +0530

    Formatting changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      About
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/Views/Header.js
+++ src/Views/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   AppBar,
   Button,
@@ -11,42 +11,42 @@ import {
   InputBase,
   Paper,
   IconButton,
-  Box
-} from '@mui/material'
-import { useState } from 'react'
-import SearchIcon from '@mui/icons-material/Search'
-import DrawerComp from '../Components/DrawerComp'
-import PropTypes from 'prop-types'
-import About from '../Components/About'
-import Home from '../Components/Home'
-import EventsV2 from '../Components/EventsV2'
-import Login from '../Pages/Login'
-import SignUp from '../Pages/SignUp'
+  Box,
+} from "@mui/material";
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import DrawerComp from "../Components/DrawerComp";
+import PropTypes from "prop-types";
+import About from "../Components/About";
+import Home from "../Components/Home";
+import EventsV2 from "../Components/EventsV2";
+import Login from "../Pages/Login";
+import SignUp from "../Pages/SignUp";
 
 const card = {
-  backgroundColor: '#38342b'
-}
+  backgroundColor: "#38342b",
+};
 const paper = {
-  display: 'flex',
-  backgroundColor: '#35383e',
-  borderRadius: '20px'
-}
+  display: "flex",
+  backgroundColor: "#35383e",
+  borderRadius: "20px",
+};
 const inputbase = {
-  backgroundColor: 'inherit',
-  color: '#fff',
-  borderRadius: '20px',
-  height: '40px',
-  width: '31vw',
-  padding: '7px'
-}
+  backgroundColor: "inherit",
+  color: "#fff",
+  borderRadius: "20px",
+  height: "40px",
+  width: "31vw",
+  padding: "7px",
+};
 
-const Pages = ['Home', 'Events', 'About US']
-function TabPanel (props) {
-  const { children, value, index, ...other } = props
+const Pages = ["Home", "Events", "About US"];
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
   return (
     <div
-      role='tabpanel'
+      role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
@@ -58,30 +58,30 @@ function TabPanel (props) {
         </Box>
       )}
     </div>
-  )
+  );
 }
 
 TabPanel.propTypes = {
   children: PropTypes.node,
   index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired
-}
+  value: PropTypes.number.isRequired,
+};
 
-function a11yProps (index) {
+function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`
-  }
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
 }
 
 const Header = () => {
-  const [value, setValue] = useState(0)
-  const theme = useTheme()
+  const [value, setValue] = useState(0);
+  const theme = useTheme();
   const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
+    setValue(newValue);
+  };
 
-  const isMatch = useMediaQuery(theme.breakpoints.down('md'))
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <>
       <AppBar sx={card}>
@@ -92,14 +92,14 @@ const Header = () => {
               <Typography>K-Event</Typography>
             </>
           ) : (
-            <div style={{ display: 'flex', justifyContent: 'left' }}>
+            <div style={{ display: "flex", justifyContent: "left" }}>
               <Typography>K-VENT MANAGER</Typography>
               <Tabs
-                style={{ marginLeft: '5vw' }}
-                textColor='inherit'
+                style={{ marginLeft: "5vw" }}
+                textColor="inherit"
                 value={value}
                 onChange={handleChange}
-                indicatorColor='secondary'
+                indicatorColor="secondary"
               >
                 {Pages.map((pages, index) => (
                   <Tab
@@ -109,14 +109,14 @@ const Header = () => {
                   ></Tab>
                 ))}
               </Tabs>
-              <Paper sx={paper} style={{ marginLeft: '5vw' }}>
+              <Paper sx={paper} style={{ marginLeft: "5vw" }}>
                 <IconButton>
-                  <SearchIcon sx={{ color: '#dcdcdc' }} />
+                  <SearchIcon sx={{ color: "#dcdcdc" }} />
                 </IconButton>
                 <InputBase
                   sx={inputbase}
-                  placeholder='Search for events'
-                  inputProps={{ 'aria-label': 'search event' }}
+                  placeholder="Search for events"
+                  inputProps={{ "aria-label": "search event" }}
                 />
               </Paper>
               <Login></Login>
@@ -135,6 +135,6 @@ const Header = () => {
         <About></About>
       </TabPanel>
     </>
-  )
-}
-export default Header
+  );
+};
+export default Header;
